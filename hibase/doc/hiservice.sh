007aecb232c8b64488087a843da58580840852ca
root <sounos@qq.com>
2014-06-11 14:46:28 +0800

    fixed doc/Makefile.am

--- hibase/doc/hiservice.sh
+++ hibase/doc/hiservice.sh
@@ -11,11 +11,11 @@ genservice()
     ln -sf hiqdocd $qdocd
     ln -sf himasterd $masterd
     popd;
-    /bin/cp -f /etc/rc.d/init.d/hidocd /etc/rc.d/init.d/$docd
-    /bin/cp -f /etc/rc.d/init.d/hindexd /etc/rc.d/init.d/$indexd
-    /bin/cp -f /etc/rc.d/init.d/hiqparserd /etc/rc.d/init.d/$parserd
-    /bin/cp -f /etc/rc.d/init.d/hiqdocd /etc/rc.d/init.d/$qdocd
-    /bin/cp -f /etc/rc.d/init.d/himasterd /etc/rc.d/init.d/$masterd
+    /bin/cp -f /etc/init.d/hidocd /etc/init.d/$docd
+    /bin/cp -f /etc/init.d/hindexd /etc/init.d/$indexd
+    /bin/cp -f /etc/init.d/hiqparserd /etc/init.d/$parserd
+    /bin/cp -f /etc/init.d/hiqdocd /etc/init.d/$qdocd
+    /bin/cp -f /etc/init.d/himasterd /etc/init.d/$masterd
     docd_httpd_port=`expr $baseport + 10`
     masterd_http_port=`expr $baseport + 20`
     indexd_http_port=`expr $baseport + 30`
@@ -60,16 +60,16 @@ genservice()
     perl -i -p -e "s|service_port = 2816|service_port = $qdocd_http_port|g" /etc/"${qdocd}.ini"
     perl -i -p -e "s|service_port = 3824|service_port = $qdocd_query_port|g" /etc/"${qdocd}.ini"
     perl -i -p -e "s|service_port = 4832|service_port = $qdocd_index_port|g" /etc/"${qdocd}.ini"
-    perl -i -p -e "s/hidoc/$doc/g" /etc/rc.d/init.d/$docd /etc/"${docd}.ini"
-    perl -i -p -e "s/HIDOC/`echo $doc|tr [a-z] [A-Z]`/g" /etc/rc.d/init.d/$docd
-    perl -i -p -e "s/hindex/$index/g" /etc/rc.d/init.d/$indexd /etc/"${indexd}.ini"
-    perl -i -p -e "s/HINDEX/`echo $index|tr [a-z] [A-Z]`/g" /etc/rc.d/init.d/$indexd
-    perl -i -p -e "s/hiqparser/$parser/g" /etc/rc.d/init.d/$parserd /etc/"${parserd}.ini"
-    perl -i -p -e "s/HIQPARSER/`echo $parser|tr [a-z] [A-Z]`/g" /etc/rc.d/init.d/$parserd
-    perl -i -p -e "s/hiqdoc/$qdoc/g" /etc/rc.d/init.d/$qdocd /etc/"${qdocd}.ini"
-    perl -i -p -e "s/HIQDOC/`echo $qdoc|tr [a-z] [A-Z]`/g" /etc/rc.d/init.d/$qdocd
-    perl -i -p -e "s/himaster/$master/g" /etc/rc.d/init.d/$masterd /etc/"${masterd}.ini"
-    perl -i -p -e "s/HIMASTER/`echo $master|tr [a-z] [A-Z]`/g" /etc/rc.d/init.d/$masterd
+    perl -i -p -e "s/hidoc/$doc/g" /etc/init.d/$docd /etc/"${docd}.ini"
+    perl -i -p -e "s/HIDOC/`echo $doc|tr [a-z] [A-Z]`/g" /etc/init.d/$docd
+    perl -i -p -e "s/hindex/$index/g" /etc/init.d/$indexd /etc/"${indexd}.ini"
+    perl -i -p -e "s/HINDEX/`echo $index|tr [a-z] [A-Z]`/g" /etc/init.d/$indexd
+    perl -i -p -e "s/hiqparser/$parser/g" /etc/init.d/$parserd /etc/"${parserd}.ini"
+    perl -i -p -e "s/HIQPARSER/`echo $parser|tr [a-z] [A-Z]`/g" /etc/init.d/$parserd
+    perl -i -p -e "s/hiqdoc/$qdoc/g" /etc/init.d/$qdocd /etc/"${qdocd}.ini"
+    perl -i -p -e "s/HIQDOC/`echo $qdoc|tr [a-z] [A-Z]`/g" /etc/init.d/$qdocd
+    perl -i -p -e "s/himaster/$master/g" /etc/init.d/$masterd /etc/"${masterd}.ini"
+    perl -i -p -e "s/HIMASTER/`echo $master|tr [a-z] [A-Z]`/g" /etc/init.d/$masterd
 
 }
 if [ $# -lt 2 ];then
