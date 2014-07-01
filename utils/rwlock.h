d4b4b878d33417addef048f6bb35e05b238eedf0
root <sounos@qq.com>
2014-07-01 20:46:56 +0800

    fixed index.c

--- utils/rwlock.h
+++ utils/rwlock.h
@@ -1,7 +1,7 @@
 #ifndef __RWLOCK__H
 #define __RWLOCK__H
 #ifdef HAVE_PTHREAD
-#define RWLOCK_INIT(x)          pthread_rwlock_init((x))
+#define RWLOCK_INIT(x)          pthread_rwlock_init((x), NULL)
 #define RWLOCK_RDLOCK(x)        pthread_rwlock_rdlock((x))
 #define RWLOCK_WRLOCK(x)        pthread_rwlock_wrlock((x))
 #define RWLOCK_UNLOCK(x)        pthread_rwlock_unlock((x))
