45937e81d28235ae9f3f9d99b0cc46775774abea
root <sounos@qq.com>
2014-08-21 10:41:10 +0800

    fixed debian.sh dh_make

--- ithunder/debian.sh
+++ ithunder/debian.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+rm -rf debian
 pkg=`grep -E "^PACKAGE_NAME=" configure |sed -e "s/PACKAGE_NAME='\(.*\)'/\1/"`
 ver=`grep -E "^PACKAGE_VERSION=" configure |sed -e "s/PACKAGE_VERSION='\(.*\)'/\1/"`
-dh_make -n -s -c bsd -e SounOS@gmail.com -p "${pkg}_${ver}"
+dh_make -y -n -s -c bsd -e SounOS@gmail.com -p "${pkg}_${ver}"
 cp control debian/control
 sed -i "s/^Package:.*/Package: $pkg/" debian/control
 sed -i "s/^Source:.*/Source: $pkg/" debian/control
 sed -i "s/^License:.*/License: $pkg/" debian/control
 sed -i "s/^Standards-Version:.*/Standards-Version: $ver/" debian/control
 sed -i "s/root <SounOS@gmail.com>/SounOS <SounOS@gmail.com>/" debian/*
+#export CFLAGS='-ggdb -O0' 
+#export CXXFLAGS='-ggdb -O0'
+#export FFLAGS='-ggdb -O0'
 dpkg-buildpackage -rfakeroot -eSounOS -mSounOS
--- libibase/debian.sh
+++ libibase/debian.sh
@@ -2,7 +2,7 @@
 rm -rf debian
 pkg=`grep -E "^PACKAGE_NAME=" configure |sed -e "s/PACKAGE_NAME='\(.*\)'/\1/"`
 ver=`grep -E "^PACKAGE_VERSION=" configure |sed -e "s/PACKAGE_VERSION='\(.*\)'/\1/"`
-dh_make -n -s -c bsd -e SounOS@gmail.com -p "${pkg}_${ver}"
+dh_make -y -n -s -c bsd -e SounOS@gmail.com -p "${pkg}_${ver}"
 cp control debian/control
 sed -i "s/^Package:.*/Package: $pkg/" debian/control
 sed -i "s/^Source:.*/Source: $pkg/" debian/control
